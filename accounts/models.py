e1cd1d93f3a0d1622980530ebf7941f5a2dd4e9e
Hanaka Iwasaki <h.iwasaki.22.9.631@4nonome.com>
2022-11-05 01:25:41 +0900

    verbose_name_plural追加しました。

--- accounts/models.py
+++ accounts/models.py
@@ -8,6 +8,12 @@ class User(AbstractUser):
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
 
+    class Meta:
+        verbose_name_plural = "ユーザー"
+
+    # https://codor.co.jp/django/how-to-use-verbose-name
+    # そもそもMetaデータとは、データを分かりやすく説明するための付帯情報
+
 
 # class FriendShip(models.Model):
 #     pass
