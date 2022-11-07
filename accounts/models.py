3a46008442cc388feb36401f5f4737ae70af2ab0
Hanaka Iwasaki <h.iwasaki.22.9.631@4nonome.com>
2022-11-07 18:05:33 +0900

    makemigrationsの実行

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    # email = models.EmailField(max_length=254)
+    email = models.EmailField(max_length=254)
     skill = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
